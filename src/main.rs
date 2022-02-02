33fc76b953544ace8fda1e27b2cefbaf0066b5d8
Elliot Konetzni <elliot@konetzni.com>
2022-02-02 20:37:05 +0100

    working loop

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,40 @@
 extern crate daemonize;
-extern crate log;
-extern crate simplelog;
 
 use daemonize::Daemonize;
-use log::info;
 use std::fs::File;
 use std::path::Path;
+use std::thread::sleep;
+use std::time;
 
 mod config;
 mod machine;
 
-use simplelog::*;
-
 fn main() {
   let cfg = config::load();
-
-  CombinedLogger::init(vec![TermLogger::new(
-    LevelFilter::Warn,
-    Config::default(),
-    TerminalMode::Mixed,
-    ColorChoice::Auto,
-  )])
-  .unwrap();
-  let stdout = File::create("/tmp/daemon.out").unwrap();
-  let stderr = File::create("/tmp/daemon.err").unwrap();
   let working_dir = Path::new(&cfg.working_dir);
+  let stdout = File::create(working_dir.join("daemon.out")).unwrap();
+  let stderr = File::create(working_dir.join("daemon.err")).unwrap();
 
   let daemonize = Daemonize::new()
-    .pid_file(working_dir.join("machine.pid")) // Every method except `new` and `start`
-    .chown_pid_file(true) // is optional, see `Daemonize` documentation
-    .working_directory(working_dir) // for default behaviour.
+    .pid_file(working_dir.join("machine.pid"))
+    .chown_pid_file(false)
+    .working_directory(working_dir)
     .user("ekonetzni")
-    .group("daemon") // Group name
-    .group(2) // or group id.
-    .umask(0o777) // Set umask, `0o027` by default.
+    .group("staff")
+    .umask(0o777)
     .stdout(stdout) // Redirect stdout to `/tmp/daemon.out`.
     .stderr(stderr) // Redirect stderr to `/tmp/daemon.err`.
-    .exit_action(|| println!("Executed before master process exits"))
     .privileged_action(|| "Executed before drop privileges");
 
   match daemonize.start() {
-    Ok(_) => loop {
-      println!("LOOPING");
-    },
+    Ok(_) => {
+      println!("Starting machine.");
+      loop {
+        let now = time::Instant::now();
+        sleep(time::Duration::from_millis(5000));
+        println!("Loop took {} seconds.", now.elapsed().as_secs());
+      }
+    }
     Err(e) => eprintln!("Error, {}", e),
   }
 }
